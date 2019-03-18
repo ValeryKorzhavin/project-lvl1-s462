6e1930a455f147cddf5e82257c244afb4815b14e
Valery Korzhavin <white.and.bushy@gmail.com>
2019-03-18 13:16:57 +0300

    step 3

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# project-lvl1-s462
+# Brain Games
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/7bfd7ea3bc7293300d57/maintainability)](https://codeclimate.com/github/ValeryKorzhavin/project-lvl1-s462/maintainability)
+
+[![Build Status](https://travis-ci.org/ValeryKorzhavin/project-lvl1-s462.svg?branch=master)](https://travis-ci.org/ValeryKorzhavin/project-lvl1-s462)
+
+
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const run = () => {
-	console.log('Welcome to the Brain Games!\n');
-	const userName = readlineSync.question(`May I have your name? `);
-	console.log(`Hello ${userName}!`);
+  console.log('Welcome to the Brain Games!\n');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello ${userName}!`);
 };
 
 export default run;
