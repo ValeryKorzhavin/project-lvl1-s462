fe6bbe9217e56ca6c815beb178d1ad5ffea20f6b
Valery Korzhavin <white.and.bushy@gmail.com>
2019-03-19 15:56:22 +0300

    implement progression game

---
+++ src/games/progression.js
@@ -0,0 +1,21 @@
+import runGame from '..';
+import generateNum, { range } from '../utils';
+
+const gameDescription = 'What number is missing in the progression?';
+const maxRandomNumber = 10;
+const progressionLength = 10;
+
+const generateGameData = () => {
+  const firstElement = generateNum(maxRandomNumber);
+  const progressionStep = generateNum(maxRandomNumber);
+  const progression = range(firstElement, progressionLength, progressionStep);
+  const hiddenElementPosition = generateNum(maxRandomNumber);
+  const rightAnswer = String(progression.splice(hiddenElementPosition, 1, '..'));
+  const question = progression.join(' ');
+
+  return { question, rightAnswer };
+};
+
+const runProgressionGame = () => runGame(gameDescription, generateGameData);
+
+export default runProgressionGame;
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
 const generateNum = maxNum => Math.floor(Math.random() * maxNum);
+const range = (first, n, step) => Array.from(new Array(n), (value, index) => first + step * index);
 
+export { range };
 export default generateNum;
