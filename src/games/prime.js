b7571a3b2575942237f7a218cbdad96040490920
Valery Korzhavin <white.and.bushy@gmail.com>
2019-03-19 16:04:23 +0300

    implement prime game

---
+++ src/games/prime.js
@@ -0,0 +1,28 @@
+import runGame from '..';
+import generateNum from '../utils';
+
+const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const maxRandomNumber = 100;
+
+const isPrime = (number) => {
+  if (number < 2) {
+    return false;
+  }
+  for (let i = 2; i <= number / 2; i += 1) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const generateGameData = () => {
+  const question = generateNum(maxRandomNumber);
+  const rightAnswer = isPrime(question) ? 'yes' : 'no';
+
+  return { question, rightAnswer };
+};
+
+const runPrimeGame = () => runGame(gameDescription, generateGameData);
+
+export default runPrimeGame;
